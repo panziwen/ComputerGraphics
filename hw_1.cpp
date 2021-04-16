ff60b7cda3388dc5113f9ee05f39dc13ee99da6e
panziwen <t108aea004@ntut.edu.tw>
2021-04-16 11:55:12 +0800

    no message

--- hw_1.cpp
+++ hw_1.cpp
@@ -11,9 +11,13 @@ void RenderScene(void);
 void buildPopupMenu();
 void myKeyboard(unsigned char, int, int);
 void mySpaceialKey(int, int, int);
+void myMouse(int, int, int x, int y);
 void drawCoordinates();
+void line();
 float tx, tz, ty;
-float x, y, z;
+float x=0, y=0, z=0;
+float posx, posy;
+int px, py;
 int main(int argc, char** argv) 
 {
 	tx = 0;
@@ -30,10 +34,11 @@ int main(int argc, char** argv)
    glutCreateWindow("Moving Cube");
    //SetupRC();
    buildPopupMenu();
-   glutReshapeFunc(ChangeSize); 
-   glutDisplayFunc(RenderScene); 
+   glutReshapeFunc(ChangeSize);
+   glutMouseFunc(myMouse);
    glutKeyboardFunc(myKeyboard);
    glutSpecialFunc(mySpaceialKey);
+   glutDisplayFunc(RenderScene);
    glutMainLoop();	
    return 0; 
 } 
@@ -124,35 +129,64 @@ void ChangeSize(int w, int h)
 } 
 void RenderScene(void) 
 {
+	double c = cos(degreex * PI / 180.0);
+	double s = sin(degreex * PI / 180.0);
+	double t = 1.0 - c;
+	// if axis is not already normalised then uncomment this
+	double magnitude = sqrt(px*px + py * py);
+
+	double cx = px / magnitude;
+	double cy = py / magnitude;
+	double cz = 0;
+
+	GLfloat m[] = {
+	1.0, 0.0, 0.0, 0.0,
+	0.0, 1.0, 0.0, 0.0,
+	0.0, 0.0, 1.0, 0.0,
+	0.0, 0.0, 0.0, 1.0 };
+
+	m[0] = c + cx * cx*t;
+	m[5] = c + cy * cy*t;
+	m[10] = c + cz * cz*t;
+	m[4] = cx * cy*t + cz * s;
+	m[1] = cx * cy*t - cz * s;
+	m[8] = cx * cz*t - cy * s;
+	m[2] = cx * cz*t + cy * s;
+	m[9] = cy * cz*t + cx * s;
+	m[6] = cy * cz*t - cx * s;
 	GLfloat translateMatrix[] = {	1.0, 0.0, 0.0, 0.0,
 									0.0, 1.0, 0.0, 0.0,
 									0.0, 0.0, 1.0, 0.0,
 									tx, ty, tz, 1.0 };
-	GLfloat rotMatrixx[] = { cos(degreey * PI / 180.0), 0.0, sin(degreey * PI / 180.0),0.0,
-							 0.0,1.0, 0.0,0.0,
-							 -sin(degreey * PI / 180.0),0.0, cos(degreey * PI / 180.0),0.0,
-							 0.0,0.0, 0.0,1.0 };//XÖáÐý×ª 
 
-	GLfloat rotMatrixy[] = { 1.0, 0.0, 0.0,0.0,
-						 0.0,cos(degreex * PI / 180.0), -sin(degreex * PI / 180.0),0.0,
-						 0.0,sin(degreex * PI / 180.0), cos(degreex * PI / 180.0),0.0,
-						 0.0,0.0, 0.0,1.0 };//YÖáÐý×ª 
+	//GLfloat rotMatrixx[] = { cos(degreey * PI / 180.0), 0.0, sin(degreey * PI / 180.0),0.0,
+	//						 0.0,1.0, 0.0,0.0,
+	//						 -sin(degreey * PI / 180.0),0.0, cos(degreey * PI / 180.0),0.0,
+	//						 0.0,0.0, 0.0,1.0 };//XÖáÐý×ª 
 
-	GLfloat rotMatrixz[] = { cos(degreez * PI / 180.0), -sin(degreez * PI / 180.0), 0.0,0.0,
-						     sin(degreez * PI / 180.0),cos(degreez * PI / 180.0), 0.0,	0.0,
-						     0.0,0.0,1.0,0.0,
-						     0.0,0.0, 0.0,1.0 };  //ZÖáÐý×ª 
+	//GLfloat rotMatrixy[] = { 1.0, 0.0, 0.0,0.0,
+	//					 0.0,cos(degreex * PI / 180.0), -sin(degreex * PI / 180.0),0.0,
+	//					 0.0,sin(degreex * PI / 180.0), cos(degreex * PI / 180.0),0.0,
+	//					 0.0,0.0, 0.0,1.0 };//YÖáÐý×ª 
+
+	//GLfloat rotMatrixz[] = { cos(degreez * PI / 180.0), -sin(degreez * PI / 180.0), 0.0,0.0,
+	//					     sin(degreez * PI / 180.0),cos(degreez * PI / 180.0), 0.0,	0.0,
+	//					     0.0,0.0,1.0,0.0,
+	//					     0.0,0.0, 0.0,1.0 };  //ZÖáÐý×ª 
+	
 	
    glClearColor(1.0, 1.0, 1.0, 1.0);
    glClearDepth(1.0f);
-   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+   glClear(GL_COLOR_BUFFER_BIT);
    glMatrixMode(GL_MODELVIEW);
    glLoadIdentity();
+   line();
    drawCoordinates();
-
-   glMultMatrixf(rotMatrixx);
+   line();
+  /* glMultMatrixf(rotMatrixx);
    glMultMatrixf(rotMatrixy);
-   glMultMatrixf(rotMatrixz);
+   glMultMatrixf(rotMatrixz);*/
+   glMultMatrixf(m);
 
    glMultMatrixf(translateMatrix);
    gluLookAt(0,0,1.0f,0,0,0,0,1,0);
@@ -218,7 +252,8 @@ void RenderScene(void)
 	//3
 
 	 
-	  glEnd();
+	  glEnd(); 
+	  glFlush();
 	  glutSwapBuffers();
 }
 void drawCoordinates()
@@ -226,17 +261,37 @@ void drawCoordinates()
 	glLineWidth(3.0f);
 	glColor3f(10.0f, 0.0f, 0.0f); 
 	glBegin(GL_LINES);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(10.0f, 0.0f, 0.0f);
+	glVertex3f(0, 0.0f, 0.0f);
+	glVertex3f(10, 0.0f, 0.0f);
 	glEnd();
 	glColor3f(0.0, 10.0, 0.0); 
 	glBegin(GL_LINES);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 10.0f, 0.0f);
+	glVertex3f(0.0f, 0, 0.0f);
+	glVertex3f(0.0f, 10, 0.0f);
 	glEnd();
 	glColor3f(0.0, 0.0, 10.0);
 	glBegin(GL_LINES);
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.0f, 10.0f);
+	glVertex3f(0, 0, 0.0f);
+	glVertex3f(0, 0, 10.0f);
 	glEnd();
 }
+void line()
+{
+	px = ((2 * posx / 400) - 1) * 10;
+	py = (1 - (2 * posy / 400)) * 10;
+	glLineWidth(3.0f);
+	glColor3f(10.0f, 0.0f, 0.0f);
+	glBegin(GL_LINES);
+	glVertex3f(0, 0.0f, 0.0f);
+	glVertex3f(px , py, 0.0f);
+	glEnd();
+}
+void myMouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		posx = x;
+		posy = y;
+	}
+}
+
